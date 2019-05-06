9b8e7e3984e7bf726252d9b3746510d4b56c3fbd
Wisn <wuyishun_kmk@outlook.com>
2019-05-06 14:36:56 +0800

    update

---
+++ app/src/main/java/com/wisn/medial/tianmao/CustomBehavior.java
@@ -0,0 +1,53 @@
+package com.wisn.medial.tianmao;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.OverScroller;
+
+import java.lang.reflect.Field;
+
+import static android.view.MotionEvent.ACTION_DOWN;
+
+/**
+ * Created by Wisn on 2019-05-06 14:24.
+ */
+public class CustomBehavior extends  AppBarLayout.Behavior {
+    private OverScroller mScroller;
+    public CustomBehavior() {
+    }
+
+    public CustomBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    // --------------begin added by shaopx -------------
+
+    @Override
+    public boolean onInterceptTouchEvent(CoordinatorLayout parent, AppBarLayout child, MotionEvent ev) {
+        if (ev.getAction() == ACTION_DOWN) {
+            Object scroller = getSuperSuperField(this, "mScroller");
+            if (scroller != null && scroller instanceof OverScroller) {
+                OverScroller overScroller = (OverScroller) scroller;
+                overScroller.abortAnimation();
+            }
+        }
+
+        return super.onInterceptTouchEvent(parent, child, ev);
+    }
+
+    private Object getSuperSuperField(Object paramClass, String paramString) {
+        Field field = null;
+        Object object = null;
+        try {
+            field = paramClass.getClass().getSuperclass().getSuperclass().getDeclaredField(paramString);
+            field.setAccessible(true);
+            object = field.get(paramClass);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return object;
+    }
+}
